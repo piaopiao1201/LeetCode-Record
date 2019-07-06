c6617bac8b7e90cc4438dd34889b47a3eaf113a5
piaopiao1201 <576883737@qq.com>
2019-07-06 13:14:42 +0800

    Code History

---
+++ 转置矩阵/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+    public int[][] transpose(int[][] A) {
+        int[][] res=new int[A[0].length][A.length];
+        for(int i=0;i<A[0].length;i++){
+            for(int j=0;j<A.length;j++){
+                res[i][j]=A[j][i];
+            }
+        }
+        return res;
+    }
+}
