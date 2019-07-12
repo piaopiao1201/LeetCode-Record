07fb62028abf447785978bf5082d08e28ca58251
piaopiao1201 <576883737@qq.com>
2019-07-12 11:43:39 +0800

    Code History

---
+++ Excel表列序号/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        int res=titleToNumber("ZY");
+        System.out.println(1);
+    }
+    public static int titleToNumber(String s) {
+        Map<Character,Integer> map=new HashMap<Character, Integer>();
+        for(char i='A';i<='Z';i++){
+            map.put(i,1+(i-'A'));
+        }
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            res+=Math.pow(26,s.length()-1-i)*map.get(s.charAt(i));
+        }
+        return res;
+    }
+}
