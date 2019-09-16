ab1409bc7d1947f752d0864e89e02bd18d23fe81
piaopiao1201 <576883737@qq.com>
2019-09-16 22:37:24 +0800

    Code History

--- 对称二叉树/src/Main.java
+++ 对称二叉树/src/Main.java
@@ -1,3 +1,5 @@
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,5 +42,18 @@ public class Main {
         listB.add(root.val);
         generateList2(listB,root.right);
         generateList2(listB,root.left);
+    }
+
+    public boolean isSymmetric2(TreeNode root){
+        return isMirror(root,root);
+    }
+    public boolean isMirror(TreeNode t1, TreeNode t2){
+        if(t1==null&&t2==null){
+            return true;
+        }
+        if(t1==null||t2==null){
+            return false;
+        }
+        return (t1.val==t2.val)&&isMirror(t1.left,t2.right)&&isMirror(t1.right,t2.left);
     }
 }
---
+++ 输出二叉树/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public List<List<String>> printTree(TreeNode root) {
+        List<List<String>> res=new ArrayList<List<String>>();
+        return res;
+    }
+    public int get
+}
